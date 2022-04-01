d04151c728cb019cb7888a270ae1b020b16e47b9
AlexAlibrandi <alexander_alibrandi@hcl.com>
2022-04-01 11:14:25 -0400

    fixed push issues

--- app/src/main/java/com/android/capstone/models/User.kt
+++ app/src/main/java/com/android/capstone/models/User.kt
@@ -10,12 +10,7 @@ data class User(
     val firstName: String = "",
     val lastName: String = "",
     val email: String = "",
-<<<<<<< HEAD
     val results: @RawValue ArrayList<ResultsModel>? = ArrayList()
-
-=======
-    val results: @RawValue ArrayList<ResultsModel> = ArrayList()
->>>>>>> f2dcfa988c2e8be128298714f78c563b50e269f6
 
 ): Parcelable
 
